b3a92ef0b03afb21f10fa2b3011d0cda9d6fe149
Xander <contact@xander479.co.uk>
2021-05-09 22:54:42 +0100

    Add methods to compare dice

--- src/xander479/Die.java
+++ src/xander479/Die.java
@@ -2,7 +2,7 @@ package xander479;
 
 import java.util.Random;
 
-public class Die {
+public class Die implements Comparable<Die> {
 	private static Random r = new Random();
 	private int value;
 	private boolean isLocked;
@@ -27,5 +27,22 @@ public class Die {
 	
 	public boolean isLocked() {
 		return isLocked;
+	}
+
+	@Override
+	public int compareTo(Die o) {
+		return this.value - o.value;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(!(o instanceof Die)) return false;
+		Die other = (Die) o;
+		return this.value == other.value;
+	}
+	
+	@Override
+	public int hashCode() {
+		return value;
 	}
 }
